c25d222e72423e2199473937cb8adb6b514b05ba
Georgios A. Tsakalos <ozmax.planet@gmail.com>
2016-07-10 23:04:36 +0300

    move navigation logic to seperate file

---
+++ Navigation.py
@@ -0,0 +1,68 @@
+class NavigationBase(object):
+
+    def __init__(self, maze, spaces, walls, goal, initial_pos):
+        self.maze = maze
+        self.spaces = spaces
+        self.walls = walls
+        if maze[initial_pos[0]][initial_pos[1]] in walls:
+            raise Exception("Starting position is on a wall")
+        else:
+            self.current_pos = initial_pos
+
+    def get_choices(self):
+        choices = {}
+        # move left
+        if ((self.current_pos[1] - 1) >= 0):
+            if self.maze[self.current_pos[0]][self.current_pos[1] - 1] \
+                    not in self.walls:
+                left = (self.current_pos[0], self.current_pos[1] - 1)
+                choices['left'] = left
+        # move right
+        if ((self.current_pos[1] + 1) <= len(self.maze[0]) - 1):
+            if self.maze[self.current_pos[0]][self.current_pos[1] + 1] \
+                     not in self.walls:
+                right = (self.current_pos[0], self.current_pos[1] + 1)
+                choices['right'] = right
+        # move up
+        if ((self.current_pos[0] - 1) >= 0):
+            if self.maze[self.current_pos[0] - 1][self.current_pos[1]] \
+                    not in self.walls:
+                up = (self.current_pos[0] - 1, self.current_pos[1])
+                choices['up'] = up
+        # move down
+        if ((self.current_pos[0] + 1) <= len(self.maze) - 1):
+            if self.maze[self.current_pos[0] + 1][self.current_pos[1]]\
+                    not in self.walls:
+                down = (self.current_pos[0] + 1, self.current_pos[1])
+                choices['down'] = down
+
+        return choices
+
+    def get_next_move(self):
+        raise NotImplemented
+
+    def move(self):
+        raise NotImplemented
+
+
+class OneDirection(NavigationBase):
+    """ Change direction when bump on obstacle"""
+    last_direction = None
+
+    def first_move(self):
+        """" first move is random """
+        choices = self.get_choices()
+        import random
+        choice = random.choice(choices.keys())
+        return choice, choices[choice]
+
+    def move(self):
+        if self.last_direction is None:
+            choice_name, choice = self.first_move()
+            self.last_direction = choice_name
+        choices = self.get_choices()
+        if self.last_direction in choices:
+            print 'going straigjt'
+            choice = choices[self.last_direction]
+        self.current_pos = choice
+        return choice
--- Robot.py
+++ Robot.py
@@ -1,36 +1,13 @@
-from maze import MAZE, BLANKS, WALLS
-
-
-def normalize_maze(maze):
-    norm_maze = []
-    for line in MAZE:
-        new_line = [char for char in line]
-        norm_maze.append(new_line)
-    return norm_maze
-
-maze = normalize_maze(MAZE)
+import copy
 
 
 class Robot(object):
 
-    visited_places = []
+    def __init__(self, maze, spaces, walls, goal, initial_pos,  Navigation):
 
-    def __init__(self, maze, spaces, walls, initial_pos):
-        self.maze = maze
-        self.spaces = spaces
-        self.walls = walls
-        if maze[initial_pos[0]][initial_pos[1]] in walls:
-            raise Exception("Starting position is on a wall")
-        else:
-            self.current_pos = initial_pos
+        self.navigation = Navigation(maze, spaces, walls, goal, initial_pos)
 
-    def choices(self):
-        pass
-
-    def random_picker(self, choices):
-        pass
-
-    def pretify_maze(self, maze):
+    def print_pretty(self, maze):
         pmaze = ''
         for line in maze:
             pline = ''.join(line)
@@ -38,12 +15,13 @@ class Robot(object):
             pmaze += pline
         return pmaze
 
-    def print_current(self):
-        maze = self.maze
-        maze[self.current_pos[0]][self.current_pos[1]] = 'X'
-        maze = self.pretify_maze(maze)
-        print maze
+    def move(self):
+        # choice = self.get_random(self.get_choices())
+        new_position = self.navigation.move()
+        self.current_pos = new_position
 
-
-robot = Robot(maze=maze, spaces=BLANKS, walls=WALLS, initial_pos=(7, 25))
-print robot.maze
+    def get_current(self):
+        maze = copy.deepcopy(self.navigation.maze)
+        maze[self.navigation.current_pos[0]][self.navigation.current_pos[1]]\
+            = 'X'
+        return self.print_pretty(maze)
