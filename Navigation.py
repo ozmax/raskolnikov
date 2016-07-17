d02983546fe521d952efe6e3e4f0adfbcc35e801
Georgios A. Tsakalos <ozmax.planet@gmail.com>
2016-07-17 15:58:05 +0300

    add dfs and bfs algorithms

--- Navigation.py
+++ Navigation.py
@@ -1,13 +1,17 @@
 class NavigationBase(object):
 
     def __init__(self, maze, spaces, walls, goal, initial_pos):
+
         self.maze = maze
         self.spaces = spaces
         self.walls = walls
+        self.goal = goal
+
         if maze[initial_pos[0]][initial_pos[1]] in walls:
             raise Exception("Starting position is on a wall")
         else:
             self.current_pos = initial_pos
+            self.initial_pos = initial_pos
 
     def get_choices(self):
         choices = {}
@@ -38,35 +42,94 @@ class NavigationBase(object):
 
         return choices
 
+    def calculate_path(self):
+        raise NotImplemented
+
+    def has_next_move(self):
+        if self.path:
+            return True
+
     def get_next_move(self):
-        raise NotImplemented
+        if self.path:
+            return self.path.pop(0)
+        else:
+            return None
 
-    def move(self):
-        raise NotImplemented
+    def print_pretty(self, maze):
+        pmaze = ''
+        for line in maze:
+            pline = ''.join(line)
+            pline += '\n'
+            pmaze += pline
+        return pmaze
 
 
-class OneDirection(NavigationBase):
-    """ Change direction when bump on obstacle"""
-    last_direction = None
+class DFS(NavigationBase):
+    stack = []
+    visited = []
 
-    def first_move(self):
-        """" first move is random """
+    def get_adjacent_unvisited(self):
+        unvisited = []
         choices = self.get_choices()
-        import random
-        choice = random.choice(choices.keys())
-        return choice, choices[choice]
+        for key, value in choices.iteritems():
+            if value not in self.visited:
+                unvisited.append(value)
+        return unvisited
 
-    def move(self):
-        if self.last_direction is None:
-            choice_name, choice = self.first_move()
-            self.last_direction = choice_name
+    def calculate_path(self):
+        self.stack.append(self.current_pos)
+        self.visited.append(self.current_pos)
+        while self.stack:
+            if self.maze[self.current_pos[0]][self.current_pos[1]] in self.goal:
+                break
+            children = self.get_adjacent_unvisited()
+            if children:
+                self.current_pos = children[0] # random.choice(children)
+                self.visited.append(self.current_pos)
+                self.stack.append(self.current_pos)
+            else:
+                self.stack.pop()
+                self.current_pos = self.stack[-1]
+
+        self.path = self.stack
+        return self.stack
+
+
+class BFS(NavigationBase):
+    visited = set()
+
+    def get_adjacent_unvisited(self):
+        unvisited = []
         choices = self.get_choices()
-        if self.last_direction in choices:
-            choice = choices[self.last_direction]
-        if self.last_direction not in choices:
-            import random
-            choice_name = random.choice(choices.keys())
-            choice = choices[choice_name]
-            self.last_direction = choice_name
-        self.current_pos = choice
-        return choice
+        for key, value in choices.iteritems():
+            if value not in self.visited and value not in self.queue:
+                unvisited.append(value)
+        return unvisited
+
+    def children_with_parent(self, children, parent):
+        return {child: parent for child in children}
+
+    def calculate_path(self):
+        history = {}
+        self.queue = [self.current_pos]
+
+        while self.queue:
+            self.current_pos = self.queue.pop(0)
+            self.visited.add(self.current_pos)
+
+            if self.maze[self.current_pos[0]][self.current_pos[1]] in self.goal:
+                break
+
+            ch = self.get_adjacent_unvisited()
+            self.queue += ch
+            history.update(self.children_with_parent(ch, self.current_pos))
+
+        pointer = self.current_pos
+        path = []
+        while pointer != self.initial_pos:
+            pointer = history[pointer]
+            path.append(pointer)
+        path.reverse()
+
+        self.path = path
+        return path
